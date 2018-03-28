5ca88ed5d8cf2b332acb2b50c1a835116bc01ccb
oriolborrellroig <oriolborrellroig@gmail.com>
2018-03-29 00:30:16 +0200

    First Login Layout

--- app/src/main/java/integra/pesintegra/MainActivity.java
+++ app/src/main/java/integra/pesintegra/MainActivity.java
@@ -1,14 +1,16 @@
 package integra.pesintegra;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_login);
         //Aixo es un test
     }
 }
