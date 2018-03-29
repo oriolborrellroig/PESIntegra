cecd7d10780b76f93f5e6cec7ba96ec537c44837
Marina Moreno Soriano <marina.moreno.soriano@fib.upc.edu>
2018-03-29 20:24:19 +0200

    register activity

--- app/src/main/java/integra/pesintegra/RegisterActivity.java
+++ app/src/main/java/integra/pesintegra/RegisterActivity.java
@@ -60,6 +60,6 @@ class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDa
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        editText.setText(day + "/" + month +"/" + year);
+        editText.setText(day + "/" + month+1 +"/" + year);
     }
 }
