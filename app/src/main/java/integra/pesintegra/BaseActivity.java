18e564f232b670506f09b5a3597d3b99842a2247
Marina Moreno Soriano <marina.moreno.soriano@fib.upc.edu>
2018-03-30 00:19:05 +0200

    navigation drawer sense toolbar

--- app/src/main/java/integra/pesintegra/BaseActivity.java
+++ app/src/main/java/integra/pesintegra/BaseActivity.java
@@ -2,12 +2,33 @@ package integra.pesintegra;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 
 public class BaseActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
