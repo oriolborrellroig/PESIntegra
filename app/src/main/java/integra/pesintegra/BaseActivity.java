c0d8322f1643642a16f6665b2f60659484925a72
oriolborrellroig <oriolborrellroig@gmail.com>
2018-03-29 02:06:29 +0200

    Testing project structure

---
+++ app/src/main/java/integra/pesintegra/BaseActivity.java
@@ -0,0 +1,13 @@
+package integra.pesintegra;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+}
--- app/src/main/java/integra/pesintegra/MainActivity.java
+++ app/src/main/java/integra/pesintegra/MainActivity.java
@@ -1,16 +1,39 @@
 package integra.pesintegra;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //Aixo es un test
+
+        Button entrar_btn = (Button)findViewById(R.id.login_entrar);
+        entrar_btn.setOnClickListener(this);
+        Button login_btn = (Button)findViewById(R.id.login_register);
+        login_btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.login_entrar:
+                intent = new Intent(getApplicationContext(),BaseActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.login_register:
+                intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
