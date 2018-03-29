b4ca5ec241eda77750688dd58766809d95f13193
oriolborrellroig <oriolborrellroig@gmail.com>
2018-03-29 11:26:58 +0200

    Cooler EditText

--- app/src/main/java/integra/pesintegra/BaseActivity.java
+++ app/src/main/java/integra/pesintegra/BaseActivity.java
@@ -7,7 +7,7 @@ public class BaseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_main);
 
     }
 }
