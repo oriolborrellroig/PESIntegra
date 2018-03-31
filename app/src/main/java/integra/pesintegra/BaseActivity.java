c613119cc2df217c5a44ba04c3dc88705ae23058
oriolborrellroig <oriolborrellroig@gmail.com>
2018-03-31 11:49:33 +0200

    Toolbar & Navigation Drawer button

--- app/src/main/java/integra/pesintegra/BaseActivity.java
+++ app/src/main/java/integra/pesintegra/BaseActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class BaseActivity extends Activity {
@@ -16,8 +17,9 @@ public class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
