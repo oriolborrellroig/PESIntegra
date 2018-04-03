b1f36959d9df513f8f098d35e3946d78c903f6c2
Daniel Tarres Amselem <tarres.amselem@est.fib.upc.edu>
2018-04-03 09:43:33 +0200

    back arreglat

--- app/src/main/java/integra/pesintegra/Presentation/AllPostsActivity.java
+++ app/src/main/java/integra/pesintegra/Presentation/AllPostsActivity.java
@@ -1,5 +1,7 @@
 package integra.pesintegra.Presentation;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 
 import integra.pesintegra.R;
 
@@ -10,8 +12,14 @@ public class AllPostsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.nav_drawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            super.onBackPressed();
+        }
     }
 
 
