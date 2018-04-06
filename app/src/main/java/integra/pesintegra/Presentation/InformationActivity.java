373daaba230724afd95cb2e930e2b25799916c1f
Victor <victorjuez.alu@gmail.com>
2018-04-06 10:00:35 +0200

    Strings d'Information Activity posats a strings.xml i els strings de contingut afegits desde la classe java

--- app/src/main/java/integra/pesintegra/Presentation/InformationActivity.java
+++ app/src/main/java/integra/pesintegra/Presentation/InformationActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import integra.pesintegra.R;
 
@@ -14,6 +16,26 @@ public class InformationActivity extends Activity implements View.OnClickListene
         setContentView(R.layout.activity_infomation);
         Button btn_back = (Button)findViewById(R.id.btn_post_back);
         btn_back.setOnClickListener(this);
+
+        /* adding TextView programmatically
+        String[] textArray = {"One", "Two", "Three", "Four"};
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        for( int i = 0; i < textArray.length; i++ )
+        {
+            TextView textView = new TextView(this);
+            textView.setText(textArray[i]);
+            linearLayout.addView(textView);
+        }
+        */
+
+        TextView emergencies = (TextView)findViewById(R.id.emergencies_txt);
+        emergencies.setText("For emergencies call 1110");
+
+        TextView hospitals = (TextView)findViewById(R.id.hospitals_txt);
+        hospitals.setText("Many of Berlin's hospitals have 24-hour emergency wards, the most central being:");
+
 
     }
 
