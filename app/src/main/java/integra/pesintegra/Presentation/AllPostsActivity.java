8cce4a78cac2d151925570a337621164e9dbce1a
Marina Moreno Soriano <marina.moreno.soriano@fib.upc.edu>
2018-04-08 23:52:00 +0200

    recycler view operativa sense connexió BD

--- app/src/main/java/integra/pesintegra/Presentation/AllPostsActivity.java
+++ app/src/main/java/integra/pesintegra/Presentation/AllPostsActivity.java
@@ -2,15 +2,44 @@ package integra.pesintegra.Presentation;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import integra.pesintegra.Logic.Adapter.ListAdapter;
+import integra.pesintegra.Logic.Clases.Post;
 import integra.pesintegra.R;
 
 public class AllPostsActivity extends BaseActivity {
+
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+
+
+        List<Post> list_posts = new ArrayList<>();
+        for (int i = 0; i < 5; ++i){
+            Post p = new Post();
+            p.setTitol("Empezamos");
+            p.setTDataIni("12/02/2018");
+            list_posts.add(p);
+        }
+        listAdapter = new ListAdapter(list_posts);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(listAdapter);
 
     }
 
