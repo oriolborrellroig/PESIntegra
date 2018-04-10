e9a4a90afc3edf6f0ccc2bdb495f5c88731c312b
Daniel Tarres Amselem <tarres.amselem@est.fib.upc.edu>
2018-04-10 11:45:24 +0200

    tres punts per poder fer el hide, etc + menu

--- app/src/main/java/integra/pesintegra/Presentation/PostActivity.java
+++ app/src/main/java/integra/pesintegra/Presentation/PostActivity.java
@@ -12,9 +12,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 
@@ -36,6 +38,31 @@ public class PostActivity extends Activity implements View.OnClickListener{
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(this);
         iv = (ImageView) findViewById(R.id.imatge);
+        final Button tres_punts = (Button) findViewById(R.id.tres_punts);
+        tres_punts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(PostActivity.this, tres_punts);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.popup_menu, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(
+                                PostActivity.this,
+                                "You Clicked : " + item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        return true;
+                    }
+                });
+
+                popup.show(); //showing popup menu
+            }
+        }); //closing the setOnClickListener method
         /*
         if (la imatge del post no és nul·la){
             iv.setImageBitmap(Bitmap.createScaledBitmap(bitmap_del_post, iv.getMaxWidth(), iv.getMaxHeight(), false));
