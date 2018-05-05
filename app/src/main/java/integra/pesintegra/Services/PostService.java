e455fff413538ceeae8c53b5accc54365122a2a5
oriolborrellroig <oriolborrellroig@gmail.com>
2018-05-05 17:51:17 +0200

    Interface Update Post

--- app/src/main/java/integra/pesintegra/Services/PostService.java
+++ app/src/main/java/integra/pesintegra/Services/PostService.java
@@ -11,6 +11,7 @@ import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -34,4 +35,7 @@ public interface PostService {
 
     @DELETE("/post/delete")
     Call<Void> deletePost(@Query("id") String value);
+
+    @PATCH("post/update")
+    Call<Void> updatePost(@Query("postid") String postid, @Body Post post);
 }
