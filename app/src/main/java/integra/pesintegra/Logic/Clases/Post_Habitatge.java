6a1d43db66065345cbd9f32d8b176fc0f157294c
Daniel Tarres Amselem <daniel.tarres@gmail.com>
2018-05-22 09:54:21 +0200

    Tags ara si que es guarden xd

--- app/src/main/java/integra/pesintegra/Logic/Clases/Post_Habitatge.java
+++ app/src/main/java/integra/pesintegra/Logic/Clases/Post_Habitatge.java
@@ -2,14 +2,16 @@ package integra.pesintegra.Logic.Clases;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
+
 public class Post_Habitatge extends Post {
 
     public Post_Habitatge(){
         super('H');
     }
 
-    public Post_Habitatge(String titol, String descripcio, String dataini, String datafi, String hora, String direccio, double lat, double lng, String lang){
-        super(titol, descripcio, dataini, datafi, hora, direccio, "1", 'H', lat, lng, lang);
+    public Post_Habitatge(String titol, String descripcio, String dataini, String datafi, String hora, String direccio, double lat, double lng, String lang, ArrayList<String> clicked_tags){
+        super(titol, descripcio, dataini, datafi, hora, direccio, "1", 'H', lat, lng, lang, clicked_tags);
         //TODO: Quan es crea un post, esta hardcodejat que l'owner es l'1
     }
 }
