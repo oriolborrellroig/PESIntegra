29fad33e7ecd0608c42d4f3ddd9cadfd976502af
mabaro3009 <mabaro3009@hotmail.com>
2018-06-15 18:32:44 +0200

    DatePickerFragment

--- app/src/main/java/integra/pesintegra/Presentation/RegisterActivity.java
+++ app/src/main/java/integra/pesintegra/Presentation/RegisterActivity.java
@@ -102,28 +102,4 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
 
 }
 
-@SuppressLint("ValidFragment")
-class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-    private EditText editText;
 
-    public DatePickerFragment(EditText dateEditText) {
-        editText = dateEditText;
-    }
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
-
-        // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-        editText.setText(day + "/" + (month+1) +"/" + year);
-    }
-}
